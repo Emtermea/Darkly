bfee26ee39a078c65914b3e0360615db07eec6d3
Emmanuelle Termeau <emtermea@student.42.fr>
2017-11-09 12:01:26 +0100

    robots_hidden script

---
+++ robots_hidden/Ressources/Readme.md
@@ -0,0 +1,6 @@
+Grace au protocole d'exclusion des robots.txt, on récupére le chemin d'un dossier nommé '/.hidden'.
+`[IP]/robots.txt`
+`[IP]/.hidden`
+Dans ce dossier sont present des liens avec des README. Avec un script `script.py` on parcourt tous les liens pour lire les README et apres quelque temps on obtient :
+
+`[IP]/.hidden/whtccjokayshttvxycsvykxcfm/igeemtxnvexvxezqwntmzjltkt/lmpanswobhwcozdqixbowvbrhw/README  :  99dde1d35d1fdd283924d84e6d9f1d820`
--- robots_hidden/Ressources/script.py
+++ robots_hidden/Ressources/script.py
@@ -2,17 +2,14 @@ import subprocess as cmd
 import re
 
 
-url = "http://10.11.200.194/.hidden/"
-
-
+url = "http://10.13.0.205/.hidden/"
 regex = 'href\=\"([^\.*].+)\"'
+readTab = []
 
 def cmd_output(com):
     pipe = cmd.Popen(com, stdout=cmd.PIPE, stderr=cmd.PIPE)
     output, errput = pipe.communicate()
     return output , errput
-
-# print(cmd_output(com)[0])
 
 def search (url):
 	com = ["curl", url]
@@ -21,15 +18,16 @@ def search (url):
 	for match in re.finditer(regex, cmd_output(com)[0]):
 		link = match.group(1)
 		links.append(link)
-	# on recupere tous les premiers liens a check
-	# on les formatte pour cmd_output
 	for elem in links:
-		elemUrl = ["curl", com[1] + elem]
-		# print elemUrl
-		#  cmd_output(elemUrl)[0]
-		for subMatch in re.finditer(regex, cmd_output(elemUrl)[0]):
-	 		subLink = elemUrl[1] + subMatch.group(1)
-			print subLink
-
+		if elem == "README":
+			retCMD = cmd_output(["curl", com[1] + elem])[0]
+			readTab.append(retCMD)
+			# if len(readTab) % 100 == 0:
+			# 	print len(readTab)
+			if re.match("\d+", retCMD):
+				print com[1] + elem, " : ", retCMD
+		else:
+			search(com[1] + elem)
 
 search(url)
+print len(readTab)
